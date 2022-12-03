a6d2cf17db512930b906b0c385505ffd66b38ed2
RamaKrishna <ram.946@gmail.com>
2022-12-03 23:18:46 +0530

    changes to py script ChrmiumService and chrometype as Chromium

--- python-selenium/demoui.py
+++ python-selenium/demoui.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.chrome import ChromeType
+# from webdriver_manager.chrome import ChromeType
+from webdriver_manager.core.utils import ChromeType
 
 driver_path = ChromeDriverManager(ChromeType.CHROMIUM).install()
 driver = webdriver.Chrome(driver_path)
+
+driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 
 driver.get("https://google.com")
 
