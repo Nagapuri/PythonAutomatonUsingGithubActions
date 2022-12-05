411e6ebaa626acb55832a415c794eba459b4b715
RamaKrishna <ram.946@gmail.com>
2022-12-05 20:03:03 +0530

    updated py rk2

--- python-selenium/demouitest.py
+++ python-selenium/demouitest.py
@@ -6,13 +6,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 from chromedriver_py import binary_path
 
-service_object = Service(binary_path)
+# service_object = Service(binary_path)
 chrome_options = webdriver.ChromeOptions()
 # chrome_options.add_argument('--headless')
 # optional
 chrome_options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(options=chrome_options,service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
-driver = webdriver.Chrome(service=service_object)
+# driver = webdriver.Chrome(options=chrome_options,service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+driver = webdriver.Chrome(options=chrome_options)
+
+# driver = webdriver.Chrome(service=service_object)
 
 driver.get("https://google.com")
 print(driver.title)
